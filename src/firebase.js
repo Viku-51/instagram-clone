553f5be65afbab8075183840e830129c2d6db473
Vikrant Chandra <58190465+Viku-51@users.noreply.github.com>
2021-07-20 15:28:33 +0530

    Create firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAWh2XLP5Gpyapsin7fQkUHNaX4Y3shozg",
+  authDomain: "instagram-clone-15d70.firebaseapp.com",
+  databaseURL: "https://instagram-clone-15d70-default-rtdb.firebaseio.com",
+  projectId: "instagram-clone-15d70",
+  storageBucket: "instagram-clone-15d70.appspot.com",
+  messagingSenderId: "897093013539",
+  appId: "1:897093013539:web:1ffd82793e663eb7d2e196",
+  measurementId: "G-WGT9PJ0MRP"
+};
